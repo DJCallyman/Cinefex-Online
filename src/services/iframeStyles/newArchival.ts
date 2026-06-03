3c30dc4188f94fd42cdca18d3f0c1547247c4a9f
Dean Calman <djcallyman@icloud.com>
2026-06-03 10:20:38 +1000

    refactor: split styleInjection module into per-feature CSS files

---
+++ src/services/iframeStyles/newArchival.ts
@@ -0,0 +1,3 @@
+import newArchivalCss from './newArchival.css?raw';
+
+export const NEW_ARCHIVAL_CSS = newArchivalCss;
---
+++ src/services/iframeStyles/oldArchival.ts
@@ -0,0 +1,3 @@
+import oldArchivalCss from './oldArchival.css?raw';
+
+export const OLD_ARCHIVAL_CSS = oldArchivalCss;
